3330f9a652cf947ba7229e6f00b2fb1efcee131c
amrithamadhavan <30970921+amrithamadhavan@users.noreply.github.com>
2018-06-01 17:09:01 +0530

    Add files via upload

--- src/main/java/com/amritha/polling/dao/PollQuestionsDao.java
+++ src/main/java/com/amritha/polling/dao/PollQuestionsDao.java
@@ -6,4 +6,10 @@ import com.amritha.polling.model.PollQuestions;
 
 public interface PollQuestionsDao {
 	public List<PollQuestions> listquestions(int id);
+
+	public void savequestion(PollQuestions question);
+
+	public PollQuestions getquestionbyid(int id);
+
+	public void deletequestion(PollQuestions question, int id);
 }
